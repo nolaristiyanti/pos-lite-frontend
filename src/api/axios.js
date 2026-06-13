6364897d8fa7b6e0dde97aa84238735061021e7b
Nola Ristiyanti <nolaristiyanti.com>
2026-06-13 07:28:19 +0700

    integrasi ke backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
-
 // const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL,
+//     baseURL: "http://127.0.0.1:8000/api",
 //     headers: {
 //         "Content-Type": "application/json",
 //     },
 // });
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
